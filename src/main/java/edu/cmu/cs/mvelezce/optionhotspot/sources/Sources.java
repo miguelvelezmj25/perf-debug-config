a6f56bf8af7d8adcf3b7cee4a73d997d0e48cf1a
Miguel Velez <miguelvelez@mijecu25.com>
2021-03-08 08:55:39 -0500

    added source methods to build sdg

---
+++ src/main/java/edu/cmu/cs/mvelezce/optionhotspot/sources/Sources.java
@@ -0,0 +1,15 @@
+package edu.cmu.cs.mvelezce.optionhotspot.sources;
+
+public class Sources {
+  public static boolean getClean(boolean b) {
+    return b;
+  }
+
+  public static int getDataSize(int i) {
+    return i;
+  }
+
+  public static int getRounds(int i) {
+    return i;
+  }
+}
