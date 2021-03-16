08fdf3923f4a8a754bf726e5eebe301bdab135c7
Miguel Velez <miguelvelez@mijecu25.com>
2021-03-16 11:44:32 -0400

    updated options' names

--- src/main/java/edu/cmu/cs/mvelezce/perf/debug/config/core/Main.java
+++ src/main/java/edu/cmu/cs/mvelezce/perf/debug/config/core/Main.java
@@ -3,11 +3,11 @@ package edu.cmu.cs.mvelezce.perf.debug.config.core;
 public class Main {
 
   public static void main(String[] args) {
-    boolean clean = false;
-    int dataSize = 10;
-    int rounds = 2;
+    boolean CLEAN = false;
+    int DATA_SIZE = 10;
+    int ROUNDS = 2;
 
-    TaskHandler taskHandler = new TaskHandler(clean, dataSize, rounds);
+    TaskHandler taskHandler = new TaskHandler(CLEAN, DATA_SIZE, ROUNDS);
     taskHandler.work();
   }
 }
